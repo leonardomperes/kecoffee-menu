d6e9247fda6199804ebfedf0a66f70941e0ec948
Leonardo Peres <leonardom.peres@gmail.com>
2025-07-07 21:48:35 -0300

    Add lanches e pratos com calabresa e especificado melhor links de acesso

--- script.js
+++ script.js
@@ -26,32 +26,39 @@ const menuItems = {
     coffees_ICE: [
         "Ice Coffee <i>(Café, Leite, Cacau, <br>Gelo e Calda)</i> - R$ 18,00"
     ],
-    snacks: [
+    snacks_PRIMARY: [
         "Pão de Queijo - R$ 6,00",
         "Salgados (Consultar Sabores) - R$ 8,00",
         "Hamburguer Assado - R$ 12,00",
+    ],
 
+    snacks_SECONDARY: [
+        "Pão na Chapa - R$ 8,00",
+        "Pão de Queijo Com Casquinha - R$ 9,00",
+        "Pão Casquinha - R$ 10,00",
+        "Pão com Ovo - R$ 12,00",
+        "Pão na Chapa com Requeijão na Saída - R$ 14,00",
+        "Misto Quente - R$ 15,00",
+        "Pão com Ovo, Queijo e Bacon - R$ 18,00",
+        "Pão com Ovo, Queijo e Calabresa - R$ 18,00",
     ],
     drinks: [
+        "Água <i>(c/ e s/gás)</i> - R$ 4,00",
         "Refrigerante <i>(Coca, Coca-Zero, Guaraná, Guaraná-Zero)</i> - R$ 6,00",
+        "Chá <i>(Consultar Sabores)</i> - R$ 7,00",
         "H20 - R$ 7,00",
         "Suco Natural <i>(Laranja, Acerola, Maracujá)<i/> - R$ 10,00",
-        "Chá <i>(Consultar Sabores)</i> - R$ 7,00",
-        "Água <i>(c/ e s/gás)</i> - R$ 4,00"
     ],
     plateOfFood: [
         "Omelete do Leo <i>(3 Ovos, Banana, Muçarela, Mel e Canela)</i> - R$ 25,00",
-        "Omelete com Queijo e Bacon - R$ 15,00",
         "Omelete Simples - R$ 10,00",
         "Omelete com Queijo - R$ 12,00",
+        "Omelete com Bacon - R$ 12,00",
+        "Omelete com Calabresa - R$ 12,00",
         "Omelete com Queijo e Bacon - R$ 15,00",
-        "Pão na Chapa - R$ 8,00",
-        "Pão Casquinha - R$ 10,00",
-        "Pão com Ovo - R$ 12,00",
-        "Pão com Ovo e Bacon - R$ 18,00",
-        "Misto Quente - R$ 15,00",
-        "Bauru - R$ 17,00",
-        "Pão de Queijo Com Casquinha - R$ 9,00",
+        "Omelete com Queijo e Calabresa - R$ 15,00",
+
+
     ],
     cakes: [
         "Bolo de Pote <i>(Consultar Sabores)</i> - R$ 21,00"
@@ -66,10 +73,12 @@ function loadMenu() {
     const coffeeArara = document.getElementById('coffee_ARARA');
     const coffeeIce = document.getElementById('coffee_ICE');
 
-    const snacksList = document.getElementById('snacks');
+    const snacksPrimaryList = document.getElementById('snacks_PRIMARY');
+    const snaksSecondaryList = document.getElementById('snacks_SECONDARY');
     const drinksList = document.getElementById('drinks');
     const plateOfFoodList = document.getElementById('plateOfFood');
     const cakesList = document.getElementById('cakes');
+
     // Formata cada item do cardápio com nome e preço alinhados
     const formatItem = (item) => {
         const [name, price] = item.split(" - "); // Divide "Nome - R$ X,XX"
@@ -87,25 +96,34 @@ function loadMenu() {
     menuItems.coffees_CATUAI.forEach(item => {
         coffeeCatuai.appendChild(formatItem(item));
     });
+
     // Preenche café NOVO MUNDO
     menuItems.coffees_NOVO_MUNDO.forEach(item => {
         coffeeNovoMundo.appendChild(formatItem(item));
     });
+
     // Preenche café BOURBON
     menuItems.coffees_BOURBON.forEach(item => {
         coffeeBourbon.appendChild(formatItem(item));
     });
+
     // Preenche café BOURBON
     menuItems.coffees_ARARA.forEach(item => {
         coffeeArara.appendChild(formatItem(item));
     });
+
     menuItems.coffees_ICE.forEach(item => {
         coffeeIce.appendChild(formatItem(item));
     });
 
-    // Preenche salgados
-    menuItems.snacks.forEach(item => {
-        snacksList.appendChild(formatItem(item));
+    // Preenche salgados PRIMARIOS
+    menuItems.snacks_PRIMARY.forEach(item => {
+        snacksPrimaryList.appendChild(formatItem(item));
+    });
+
+    // Preenche salgados SECONDARIOS (Lanches)
+    menuItems.snacks_SECONDARY.forEach(item => {
+        snaksSecondaryList.appendChild(formatItem(item));
     });
 
     // Preenche bebidas
